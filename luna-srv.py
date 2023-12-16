5ea03f16571b66723afddb27b9b6e0b265e6e8ed
Alex Lévy <alexlevy0@gmail.com>
2023-12-16 14:09:14 +0100

    try with test files

--- luna-srv.py
+++ luna-srv.py
@@ -2,9 +2,20 @@ import time
 
 
 def main():
-    for i in range(1):
-        print(f"Ligne {i}: contenu de test")
-        time.sleep(1)  # Attendre 1 seconde entre chaque ligne
+    video_filename = "video.mp4"
+    image_filename = "moi.jpg"
+
+    output_filename = (
+        f"{video_filename.split('.')[0]}-{image_filename.split('.')[0]}.mp4"
+    )
+
+    command = f"python run.py -s '{image_filename}' -t '{video_filename}' -o '{output_filename}' --keep-fps --output-video-quality 10"
+    # command = "python run.py -h"
+
+    process = subprocess.Popen(
+        command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+    # output, error = process.communicate()
 
 
 if __name__ == "__main__":
