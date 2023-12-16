721d4f9355a0a27c110c56f70e0f77a96a9ca2d6
Alex Lévy <alexlevy0@gmail.com>
2023-12-16 14:26:16 +0100

    try with -h

--- luna-srv.py
+++ luna-srv.py
@@ -9,13 +9,10 @@ def main():
         f"{video_filename.split('.')[0]}-{image_filename.split('.')[0]}.mp4"
     )
 
-    command = f"python run.py -s '{image_filename}' -t '{video_filename}' -o '{output_filename}' --keep-fps --output-video-quality 10"
-    # command = "python run.py -h"
+    # command = f"python run.py -s '{image_filename}' -t '{video_filename}' -o '{output_filename}' --keep-fps --output-video-quality 10"
+    command = "python run.py -h"
 
-    process = subprocess.Popen(
-        command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-    )
-    # output, error = process.communicate()
+    process = subprocess.Popen(command)
 
 
 if __name__ == "__main__":
